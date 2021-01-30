d7a544dfe313fbcbce151dff89a1b0d8a31c3488
josepaulo <josepaulo@ceuma.br>
2021-01-30 14:11:05 -0300

    mudança pra log de testes

--- uTests/check.js
+++ uTests/check.js
@@ -1,2 +1,9 @@
 
-Check = checks => !checks.some(check => !check);//true somente não tiver nenhum false
+Check = checks => {
+    !checks.some(check => {
+        if(check[0] != check[1]){
+            console.log(check[0] +" != "+check[1]);
+        }
+        return check[0] != check[1]
+    });//true somente não tiver nenhum false
+}
--- uTests/ublocos.js
+++ uTests/ublocos.js
@@ -1,7 +1,7 @@
 panel_dom = document.getElementById("#panel")
 
 function runBlocosTests(context) {
-    console.assert(resetWorks.call(context), "resetWorks");
+    //console.assert(resetWorks.call(context), "resetWorks");
     console.assert(contentObedeceIndex.call(context), "contentObedeceIndex");
 }
 
@@ -10,7 +10,7 @@ function resetWorks(){
     this.reset();
 
     checks = []
-    checks.push(this.content.length == 0)
+    checks.push([this.getContent.length, 0])
 
     return Check(checks);
 }
@@ -19,8 +19,8 @@ function contentObedeceIndex(){
     this.goTo(0, 3);//capitulo 0, bloco 3, titulo mais 2 blocos opcionais
     checks = []
 
-    checks.push(this.content[0].titulo == "Prologo")
-    checks.push(this.content.length == 3)
+    checks.push([this.getPaginaInicial.titulo, "Prologo"])
+    checks.push([this.getContent.length, 2])
 
     return Check(checks);
 }
