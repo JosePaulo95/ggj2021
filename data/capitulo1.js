4eba49ddbe5d607f69c6561de66eeaeacb18db2a
josepaulo <josepaulo@ceuma.br>
2021-01-31 04:07:26 -0300

    arquivos de caps

---
+++ data/capitulo1.js
@@ -0,0 +1,102 @@
+const _capitulo1 = {
+    "0": {
+      "titulo": "Capítulo 1",
+      "subtitulo": "O salão de Goro"
+    },
+    "1": {
+      "tipo": "narracao",
+      "fala": "chegam ao grande salão de Goro, o pai de Róssia um dos 2 reis do inferno"
+    },
+    "2" : {
+      "autor": "Goro",
+      "fala": "Róssia, minha filha. Bem-vinda!",
+      "next": 200
+    },
+    "200" : {
+      "autor": "Róssia",
+      "fala": "Amado pai!",
+      "next": "3"
+    },
+    "3": {
+      "tipo": "escolha-fala",
+      "index_opcoes": [4, 5]
+    },
+    "4": {
+      "tipo": "narracao",
+      "fala": "Jobin aguarda em silêncio",
+      "next": 7
+    },
+    "5" : {
+      "autor": "jobin",
+      "fala": "Oi pessoal, boa noite!"
+    },
+    "6" : {
+      "autor": "guarda",
+      "fala": "Não fale sem permissão na presença do rei!"
+    },
+    "7" : {
+      "autor": "Róssia",
+      "fala": "Papai, este é meu noivo."
+    },
+    "8" : {
+      "autor": "Goro",
+      "fala": "Mas não é possível! Meu tesouro, nós dois sabemos como isso termina.",
+      "next": 11
+    },
+    "11" : {
+      "autor": "Róssia",
+      "fala": "Sei porque diz isso. Vá em frente, teste-o.",
+      "next": "12"
+    },
+    "12" : {
+      "autor": "Goro",
+      "fala": "Mortal, fale.",
+      "next": 14
+    },
+    "14" : {
+      "autor": "Jobin",
+      "fala": "Eu sou o Jobin, senhor."
+    },
+    "15" : {
+      "autor": "Goro",
+      "fala": "Veja, seja você humano ou demônio, para casar com minha filha, você precisa provar seu valor."
+    },
+    "16": {
+      "tipo": "escolha-fala",
+      "index_opcoes": [17, 19]
+    },
+    "17" : {
+      "autor": "Jobin",
+      "fala": "Eu estou disposto a lutar, senhor."
+    },
+    "18" : {
+      "autor": "Goro",
+      "fala": "Você não é um soldado para eu medir seu valor em combate.",
+      "next": 21
+    },
+    "19" : {
+      "autor": "Jobin",
+      "fala": "Eu trago presentes, senhor."
+    },
+    "20" : {
+      "autor": "Goro",
+      "fala": "De pó já não é suficiente você, garoto?",
+      "next": 21
+    },
+    "21" : {
+      "autor": "Goro",
+      "fala": "Faça o seguinte: prove que você tem valor entre demônios. Neste mundo, reinam eu e meu irmão. Vá ao reino dele e traga seu tesouro mais precioso. Consiga-o e você será um mortal com os 2 tesouros mais preciosos do inferno."
+    },
+    "22" : {
+      "autor": "Róssia",
+      "fala": "Vá, sofra e volte para mim vitorioso."
+    },
+    "23" : {
+      "autor": "Jobin",
+      "fala": "Sacramentaremos nossa união com meu sangue."
+    },
+    "24" : {
+      "tipo": "narracao",
+      "fala": "parte. Fim do capítulo 1"
+    }
+  }
---
+++ data/prologo.js
@@ -0,0 +1,67 @@
+const _prologo = {
+    "0": {
+      "titulo": "Prólogo",
+      "subtitulo": "Um casal ao pôr do sol",
+      "next": 1
+    },
+    "1": {
+      "autor": "Róssia",
+      "fala": "💋💋💋"
+    }, 
+    "2": {
+      "autor": "Jobin",
+      "fala": "💜💜💜"
+    }, 
+    "3": {
+      "autor": "Róssia",
+      "fala": "Jobin, para um humano, até que você me surpreende."
+    }, 
+    "4": {
+      "autor": "Jobin",
+      "fala": "Não posso dizer o mesmo de você, Róssia. Você é tudo que eu esperava."
+    }, 
+    "5": {
+      "autor": "Róssia",
+      "fala": "Ah, Jobin! Precisamos da benção de meu pai para ficarmos juntos!"
+    }, 
+    "6": {
+      "autor": "Jobin",
+      "fala": "Eu serei digno dela, mesmo que eu tenha que enfrentar o inferno e a terra."
+    }, 
+    "7": {
+      "autor": "Róssia",
+      "fala": "Jobin, você consegue ver a beleza que há na escuridão. Case-se comigo."
+    },
+    "8": {
+      "tipo": "escolha-fala",
+      "index_opcoes": [9, 10]
+    },
+    "9": {
+      "autor": "Jobin",
+      "fala": "Róssia, na luz do mundo me perco, mas na sua escuridão é que me encontro. Eu serei seu esposo.",
+      "next": 11
+    }, 
+    "10": {
+      "autor": "Jobin",
+      "fala": "Pode crer.",
+      "next": "14" 
+    },
+    "11": {
+      "autor": "Róssia",
+      "fala": "Levante-se, precisamos ir ao inferno pedir a benção de meu pai. Prepare alguns presentes e inspire coragem."
+    },
+    "12":{
+      "autor": "Jobin",
+      "fala": "Vamos."
+    },
+    "13": {
+      "tipo": "narracao",
+      "fala": "partem. Fim do prólogo.",
+      "happy_end": true
+    },
+    "14": {
+      "tipo": "narracao",
+      "fala": "Jobin morre",
+      "dead_end": true
+    }
+  }
